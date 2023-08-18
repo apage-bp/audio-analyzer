006141ceb95774dd7737e3c82bfd846a5074a44e
Alex Page <79941788+apage-bp@users.noreply.github.com>
2023-08-18 09:06:58 -0400

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -1,5 +1,4 @@
 import streamlit as st
-#import logging
 import modal
 import json
 import os
@@ -7,7 +6,6 @@ import re
 
 def main():
     st.title("Newsletter Dashboard")
-
     available_podcast_info = create_dict_from_json_files('.')
 
     # Left section - Input fields
@@ -16,7 +14,6 @@ def main():
     # Dropdown box
     st.sidebar.subheader("Available Podcasts Feeds")
     selected_podcast = st.sidebar.selectbox("Select Podcast", options=available_podcast_info.keys())
-
     if selected_podcast:
         podcast_info = available_podcast_info[selected_podcast]
         display_podcast_info(podcast_info)
@@ -26,88 +23,35 @@ def main():
     url = st.sidebar.text_input("Link to RSS Feed")
     process_button = st.sidebar.button("Process Podcast Feed")
     st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
-
+    
     if process_button:
-
-        # Call the function to process the URLs and retrieve podcast guest information
         podcast_info = process_podcast_info(url)
-
-        # Right section - Newsletter content
-        st.header("Newsletter Content")
-
-        # Display the podcast title
-        st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
-
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
-
-        # Use regex to remove the guest details from the podcast_summary
-        cleaned_summary = re.sub(r'GUEST NAME: .*?GUEST TITLE: [a-ZA-Z\.]+', '', podcast_info['podcast_summary'])
-        print(cleaned_summary)
-
-        # Display the podcast summary and the cover image in a side-by-side layout
-        with col1:
-            st.subheader("Podcast Episode Summary")
-            st.write(cleaned_summary)  # Use the cleaned summary
-        
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-            
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
-
-        header_text = "Podcast Guest" if podcast_info['podcast_guest']['title'] != 'Host' else "Podcast Host"
-        details_header_text = header_text + " Details"
-        
-        with col3:
-            st.subheader(header_text)
-            st.write(podcast_info['podcast_guest']['name'])
-        
-        with col4:
-            st.subheader(details_header_text)
-            st.write(podcast_info["podcast_guest"]['summary'])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        display_podcast_info(podcast_info)
 
 def display_podcast_info(podcast_info):
-    # Right section - Newsletter content
     st.header("Newsletter Content")
     st.subheader("Episode Title")
     st.write(podcast_info['podcast_details']['episode_title'])
 
     # Use regex to remove the guest details from the podcast_summary
-    print(podcast_info)
     match = re.search(r'GUEST TITLE: .*?\n\n(.*)', podcast_info['podcast_summary'], flags=re.DOTALL)
-    if match:
-        cleaned_summary = match.group(1)
-    else:
-        cleaned_summary = podcast_info['podcast_summary']
-    print("cleansed: "+cleaned_summary)
-    
+    cleaned_summary = match.group(1) if match else podcast_info['podcast_summary']
+
     # Display the podcast summary and the cover image in a side-by-side layout
     col1, col2 = st.columns([7, 3])
     with col1:
         st.subheader("Podcast Episode Summary")
-        st.write(cleaned_summary)  # Use the cleaned summary
-    
+        st.write(cleaned_summary)
     with col2:
         st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
     # Display the podcast guest and their details in a side-by-side layout
-    header_text = "Podcast Guest" if podcast_info['podcast_guest']['title'] != 'Host' else "Podcast Host"
+    header_text = "Podcast Guest" if podcast_info['podcast_details']['episode_title'] != 'Host' else "Podcast Host"
     details_header_text = header_text + " Details"
-
     col3, col4 = st.columns([3, 7])
     with col3:
         st.subheader(header_text)
         st.write(podcast_info['podcast_guest']['name'])
-    
     with col4:
         st.subheader(details_header_text)
         st.write(podcast_info["podcast_guest"]['summary'])
@@ -116,21 +60,17 @@ def display_podcast_info(podcast_info):
     st.subheader("Key Moments")
     key_moments = podcast_info['podcast_highlights']
     for moment in key_moments.split('\n'):
-        st.markdown(
-            f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
     data_dict = {}
-
     for file_name in json_files:
         file_path = os.path.join(folder_path, file_name)
         with open(file_path, 'r') as file:
             podcast_info = json.load(file)
             podcast_name = podcast_info['podcast_details']['podcast_title']
-            # Process the file data as needed
             data_dict[podcast_name] = podcast_info
-
     return data_dict
 
 def process_podcast_info(url):
